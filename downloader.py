dbf067043b64b7aeb95485d940f4744bf47cadf3
Alex <alex@scotland.ca>
2019-09-14 18:45:25 +0000

    Creation of actual scrape

--- downloader.py
+++ downloader.py
@@ -1,4 +1,4 @@
-import urllib.request, requests, time, wget, random, os, zipfile, sys, re ,io, smtplib, psycopg2
+import urllib.request, requests, time, random, os, zipfile, sys, re ,io, smtplib, psycopg2
 from requests.auth import HTTPBasicAuth
 from urllib.parse import urlencode, quote_plus
 from bs4 import BeautifulSoup
@@ -6,21 +6,20 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from helper import *
-from database import *
 
 class seleniumBrowser:
     def __init__(self):
-        self.diva_db = db()
         self.options = Options()
+        self.cap = DesiredCapabilities().FIREFOX
+        self.cap["marionette"] = True
         self.options.headless = True
-        #self.options.log.level = "trace" #other options {fatal, error, warn, info(default), config, debug, trace}
-        self.driver = webdriver.Firefox(options=self.options)
+        self.driver = webdriver.Firefox(executable_path='/home/Dug/chorus_downloader/geckodriver', capabilities=self.cap, options=self.options)
         self.driver.delete_all_cookies()
         self.driver.implicitly_wait(2)
 
     def __del__(self):
-        self.diva_db.logout()
         self.driver.quit()
 
     def getQuery(self,search_term):
@@ -31,3 +30,36 @@ class seleniumBrowser:
             print(msg)
         else:
             return True
+
+    def getFirstSong(self):
+        try:
+            song_charter = []
+            requested_song_name = 'Nightmare'
+            list_of_songs=self.driver.find_elements_by_class_name("Song")
+            for i in list_of_songs:
+                song_element = i.find_element_by_class_name("Song__name")
+                song_name = song_element.get_attribute("innerText")
+                if song_name == requested_song_name:
+                    try:
+                        song_charter= i.find_element_by_class_name('DownloadLink')
+                    except Exception as msg:
+                        print(msg)
+                        song_charter= i.find_element_by_class_name('DownloadLink--verified')
+                    finally:
+                        link = song_charter.find_element_by_tag_name('a')
+                        link=link.get_attribute('href')
+                        if 'https://public.fightthe.pw' in link:
+                            self.driver.get(link)
+                        elif 'drive.google' in link:
+                            uid = getGoogleID(link)
+                            self.driver.get('https://drive.google.com/uc?export=download&id='+str(uid))
+                        break
+        except Exception as msg:
+            print(msg)
+        else:
+            return True
+
+
+song_finder=seleniumBrowser()
+if song_finder.getQuery("Nightmare by Avenged Sevenfold"):
+    song_finder.getFirstSong()
--- helper.py
+++ helper.py
@@ -1,4 +1,4 @@
-import urllib.parse
+import urllib.parse, re
 
 def generateUrl(search_term):
 	try:
@@ -7,3 +7,21 @@ def generateUrl(search_term):
 		print(msg)
 	else:
 		return 'https://chorus.fightthe.pw/search?query='+str(new_term)
+
+def getGoogleID(link):
+	try:
+		letter_count = 0
+		uid= ''
+		for i in link:
+			if i == '/':
+				letter_count = 0
+				uid = ''
+			else:
+				letter_count += 1
+				uid += i
+			if letter_count == 33:
+				break
+	except:
+		print('eer')
+	else:
+		return uid
