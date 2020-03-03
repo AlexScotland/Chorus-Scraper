7cf98a066d9964bd4facad0ecd0a2ddee5cb3eb9
Alex <alex@scotland.ca>
2020-03-03 01:58:57 -0500

    Beginning of deprecation of Selenium, using PyQt5 and PyQtWebEngine

--- cron-me.py
+++ cron-me.py
@@ -1,21 +1,39 @@
 from downloader import *
 sys.path.append("/usr/share/pyshared/song_request/python-files/")
-from database import db
+##from database import db
 
 if __name__ == '__main__':
-    cdb = db()
-    cdb.login()
-    for i in cdb.getAllReqSongs():
-        try:
-            song = i[1]
-            artist = i[2]
-            requester = i[3]
-            song_finder = seleniumBrowser()
-            if song_finder.getQuery(str(song)+" by "+str(artist)):
-                song_finder.getFirstSong(song)
-                song_finder.quit()
-        except Exception as msg:
-            print(msg)
-        finally:
-            cdb.removeReqSongbyReq(requester)
-            cdb.logout()
+    song=["","Through the Fire and the Flames","DragonForce","Dug"]
+    songs = song[1]
+    artist = song[2]
+    requester = song[3]
+    song_finder = songDownloader()
+    try:
+        song_finder.getFirstSong(song_finder.getQuery(str(songs)+" by "+str(artist)),song)
+    except Exception as msg:
+        print('err')
+        print(msg)
+    else:
+        song_finder.quit()
+
+##    cdb = db()
+##    cdb.login()
+##    for i in cdb.getAllReqSongs():
+##        try:
+##            song = i[1]
+##            artist = i[2]
+##            requester = i[3]
+##            song_finder = songDownloader()
+##            try:
+##                song_finder.getFirstSong(song_finder.getQuery(str(song)+" by "+str(artist)),song)
+##            except Exception as msg:
+##                print('err')
+##                print(msg)
+##            else:
+##                song_finder.quit()
+##            
+##        except Exception as msg:
+##            print(msg)
+##        finally:
+##            cdb.removeReqSongbyReq(requester)
+##            cdb.logout()
--- downloader.py
+++ downloader.py
@@ -1,77 +1,57 @@
-import urllib, urllib.request, requests, time, random, os, zipfile, sys, re ,io, smtplib, psycopg2
+import urllib, urllib.request, requests, time, random, sys, os, zipfile, re ,io, smtplib, psycopg2
 from requests.auth import HTTPBasicAuth
 from urllib.parse import urlencode, quote_plus
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from helper import *
-
-class seleniumBrowser:
-    def __init__(self):
-        self.options = Options()
-        self.cap = DesiredCapabilities().FIREFOX
-        self.cap["marionette"] = True
-        self.options.headless = True
-        self.profile = webdriver.FirefoxProfile()
-        self.profile.set_preference('browser.download.folderList', 2) # custom location
-        self.profile.set_preference('browser.download.manager.showWhenStarting', False)
-        self.profile.set_preference('browser.download.dir','/home/Dug/songs_downloaded/')
-        self.profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/zip, application/rar')
-        self.profile.set_preference("pdfjs.disabled", True)
-        self.profile.set_preference("browser.helperApps.alwaysAsk.force", False)
-        self.driver = webdriver.Firefox(self.profile,executable_path='/usr/share/pyshared/chorus_downloader/geckodriver', service_log_path='/dev/null',capabilities=self.cap, options=self.options)
-        self.driver.delete_all_cookies()
-        self.driver.implicitly_wait(2)
-
+from webpageLoader import render
+class songDownloader:
     def __del__(self):
-        self.driver.quit()
-
-    def quit(self):
         self.driver.quit()
 
     def getQuery(self,search_term):
         try:
             link=generateUrl(search_term)
-            self.driver.get(link)
         except Exception as msg:
             print(msg)
         else:
-            return True
+            print(link)
+            return link
 
-    def getFirstSong(self,request):
+    def getFirstSong(self,link,request):
         try:
             song_charter = []
             requested_song_name = request
-            list_of_songs=self.driver.find_elements_by_class_name("Song")
+            html_raw = render(link)
+            scraper=BeautifulSoup(html_raw)
+            list_of_songs= scraper.findAll("div", {"class": "Song"})
             for i in list_of_songs:
                 breaker = False
-                song_element = i.find_element_by_class_name("Song__name")
-                song_name = song_element.get_attribute("innerText")
-                try:
-                    song_charter= i.find_element_by_class_name('DownloadLink')
-                except Exception as msg:
-                    song_charter= i.find_element_by_class_name('DownloadLink--verified')
-                finally:
-                    link = song_charter.find_element_by_tag_name('a')
-                    link=link.get_attribute('href')
-                    if 'https://public.fightthe.pw' in link:
-                        self.driver.get(link)
-                        os.system('wget '+link+' -P /home/Dug/songs_downloaded/')
-                    elif 'drive.google' in link:
-                        uid = getGoogleID(link)
-                        url = 'https://drive.google.com/uc?export=download&id='+str(uid)
-                        r = requests.get(url)
-                        if r.status_code == 404:
-                            breaker = False
-                        elif r.status_code == 200:
-                            breaker = True
-                            file_name = extractName(r.headers['Content-Disposition'])
-                            urllib.request.urlretrieve(url, '/home/Dug/songs_downloaded/'+str(file_name))
-                            # self.driver.get('https://drive.google.com/uc?export=download&id='+str(uid))
-                    if breaker:
+                song_element = i.findAll("div", {"class": "Song__name"})
+                print(song_element)
+                song_name = song_element[0].text
+                print(song_name)
+##                try:
+##                    song_charter= i.find_element_by_class_name('DownloadLink')
+##                except Exception as msg:
+##                    song_charter= i.find_element_by_class_name('DownloadLink--verified')
+##                finally:
+##                    link = song_charter.find_element_by_tag_name('a')
+##                    link=link.get_attribute('href')
+##                    if 'https://public.fightthe.pw' in link:
+##                        self.driver.get(link)
+##                        os.system('wget '+link+' -P /home/Dug/songs_downloaded/')
+##                    elif 'drive.google' in link:
+##                        uid = getGoogleID(link)
+##                        url = 'https://drive.google.com/uc?export=download&id='+str(uid)
+##                        r = requests.get(url)
+##                        if r.status_code == 404:
+##                            breaker = False
+##                        elif r.status_code == 200:
+##                            breaker = True
+##                            file_name = extractName(r.headers['Content-Disposition'])
+##                            urllib.request.urlretrieve(url, '/home/Dug/songs_downloaded/'+str(file_name))
+##                            # self.driver.get('https://drive.google.com/uc?export=download&id='+str(uid))
+##                    if breaker:
                         break
         except Exception as msg:
             print(msg)
